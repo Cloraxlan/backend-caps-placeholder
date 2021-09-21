493333de7ac41f95546c5e6ea56f637aab8affad
Cloraxlan <sbluek546@gmail.com>
2021-09-21 18:44:26 -0500

    generate session tokens

--- processes/account/createAccount.ts
+++ processes/account/createAccount.ts
@@ -1,9 +1,11 @@
 import User from "../../interfaces/User";
 import { connection } from "../mongoConnection";
+import { generateToken } from "./generateSession";
 
 const DATABASE = "caps-placeholder";
 const COLLECTION = "Users";
 export const createAccount = async (user: User) => {
 	let userConnection = (await connection).db(DATABASE).collection(COLLECTION);
+	user.tokens.push(generateToken());
 	userConnection.insertOne(user);
 };
--- processes/account/generateSession.ts
+++ processes/account/generateSession.ts
@@ -1,1 +1,24 @@
-//yeah this is a todo for now
+import User, { SessionToken } from "../../interfaces/User";
+import { v4 as uuidv4, v4 } from "uuid";
+
+//2 weeks default duration
+export const generateToken: (duration?: number) => SessionToken = (
+	duration: number = 1000 * 60 * 60 * 24 * 7,
+) => {
+	console.log(new Date().getTime());
+	console.log(duration);
+	return {
+		expires: new Date().getTime() + duration,
+		value: v4(),
+	};
+};
+
+export default class Session {
+	private _user: User;
+	constructor(user: User) {
+		this._user = user;
+	}
+	get user(): User {
+		return this._user;
+	}
+}
