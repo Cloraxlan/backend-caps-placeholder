1fb7579cacee7ef9c1417b290cb63d25ee51d40c
Cloraxlan <sbluek546@gmail.com>
2021-09-21 19:01:50 -0500

    probably should make a branch, incomplete stuff

--- processes/account/Session.ts
+++ processes/account/Session.ts
@@ -12,13 +12,24 @@ export const generateToken: (duration?: number) => SessionToken = (
 		value: v4(),
 	};
 };
-
+//TODO, use only token....
 export default class Session {
 	private _user: User;
-	constructor(user: User) {
+	private _expired: boolean;
+	private _token: SessionToken;
+	constructor(user: User, token: SessionToken) {
 		this._user = user;
+		this._token = token;
+		if (token.expires > new Date().getTime()) {
+			this._expired = false;
+		} else {
+			this._expired = true;
+		}
 	}
 	get user(): User {
 		return this._user;
+	}
+	get expired(): boolean {
+		return this._expired;
 	}
 }
