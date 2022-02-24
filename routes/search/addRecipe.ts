27c1b2b4417d43c0bf8f29bbbf6438af24aceb7c
Konrad <sbluek546@gmail.com>
2022-02-24 11:11:55 -0600

    test

--- routes/search/addRecipe.ts
+++ routes/search/addRecipe.ts
@@ -12,8 +12,7 @@ export default Router().post("/", async (req, res) => {
 	};
 	console.log(req.body);
 	try {
-		console.log(req.body.recipe);
-		add(JSON.parse(req.body.recipe) as serialRecipe);
+		await add(JSON.parse(req.body.recipe) as serialRecipe);
 		res.json(metadata);
 	} catch (error) {
 		errorMessage(metadata, res, error);
