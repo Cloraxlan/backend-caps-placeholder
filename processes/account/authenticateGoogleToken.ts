1ec1405e34553dfa66651c431d6add4ee9efd786
Konrad <sbluek546@gmail.com>
2022-03-09 11:50:13 -0600

    tst

--- processes/account/authenticateGoogleToken.ts
+++ processes/account/authenticateGoogleToken.ts
@@ -1,19 +1,10 @@
-const { OAuth2Client } = require("google-auth-library");
-const CLIENT_ID =
-	"201437708650-9ndfuhshviue7au27pa3e3me4vrqlhu5.apps.googleusercontent.com";
 export const authenticateGoogleToken = async (token: string) => {
-	const client = new OAuth2Client(CLIENT_ID);
-	async function verify() {
-		const ticket = await client.verifyIdToken({
-			idToken: token,
-			audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
-			// Or, if multiple clients access the backend:
-			//[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-		});
-		const payload = ticket.getPayload();
-		const userid = payload["sub"];
-		// If request specified a G Suite domain:
-		// const domain = payload['hd'];
-	}
-	verify().catch(console.error);
+	let res = await fetch(
+		`https://www.googleapis.com/oauth2/v3/userinfo?access_token="` +
+			token +
+			`"`,
+	);
+	let json = await res.json();
+	console.log(json.email);
+	return json.email;
 };
